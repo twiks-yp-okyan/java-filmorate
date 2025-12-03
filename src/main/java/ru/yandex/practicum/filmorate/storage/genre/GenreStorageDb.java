ca2ee465f86c78092be2f40bff6eac7e2204e42c
twiksokyan <twkscode@gmail.com>
2025-12-03 20:12:59 +0300

    fix for checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorageDb.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorageDb.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.genre;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import ru.yandex.practicum.filmorate.model.FilmGenre;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.BaseRepository;
 
