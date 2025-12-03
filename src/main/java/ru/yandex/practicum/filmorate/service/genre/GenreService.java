6e391f46f54b85053c5815cca4ca7ca77b87285f
twiksokyan <twkscode@gmail.com>
2025-12-04 01:16:49 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.genre.FilmGenreStorage;
 import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
