6d132b055fadfc3a76a23f1e9218d1f71f346298
twiksokyan <twkscode@gmail.com>
2025-12-03 20:31:48 +0300

    fix for checkstyle #9

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
