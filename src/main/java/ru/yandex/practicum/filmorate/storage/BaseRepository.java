d83d3412443cc5d7133c4c17993a0f01e175e7aa
twiksokyan <twkscode@gmail.com>
2025-12-03 20:24:18 +0300

    fix for checkstyle #6

--- src/main/java/ru/yandex/practicum/filmorate/storage/BaseRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/BaseRepository.java
@@ -50,7 +50,8 @@ public class BaseRepository<T> {
             for (int idx = 0; idx < params.length; idx++) {
                 ps.setObject(idx + 1, params[idx]);
             }
-            return ps;}, keyHolder);
+            return ps;
+            }, keyHolder);
 
         Long id = keyHolder.getKeyAs(Long.class);
 
