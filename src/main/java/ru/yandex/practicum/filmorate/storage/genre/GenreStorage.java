28b723001e6256a5ababadb5fd9f9e6d6f5bf03f
twiksokyan <twkscode@gmail.com>
2025-12-03 20:30:06 +0300

    fix for checkstyle #8

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.genre;
 
-import ru.yandex.practicum.filmorate.model.FilmGenre;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.Collection;
